e9df67bd7b8a0dc47816067d8cf98bc8c5bcbe7e
Ekaterina Budon <ekaterinabudon@gmail.com>
2019-06-19 17:07:53 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <RaiseToExp></RaiseToExp>
+  </div>
+</template>
+
+<script>
+import RaiseToExp from './components/RaiseToExp.vue'
+
+export default {
+  name: 'app',
+  components: {
+    RaiseToExp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/RaiseToExp.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1>Возведение в степень</h1>
+    <button @click="increment">⬆︎</button>
+    <input type="text" v-model="total" @change="updateArray">
+    <ul>
+      <li v-for="item in arrayOfExps" :key="item.exp">{{base}}<sup>{{item.exp}}</sup> = {{item.result}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      total: 5,
+      base: 2,
+      arrayOfExps: [],
+    }
+  },
+  methods: {
+   increment: function() {
+     this.total = this.total + 1;
+     this.updateArray();
+   },
+   updateArray: function() {
+     const array = [];
+     for (let i = 0; i < this.total; i++) {
+       const exp = 10 + i;
+       array.push({
+         exp,
+         result: Math.pow(this.base, exp)
+       })
+
+     }
+     this.arrayOfExps = array;
+
+   }
+ },
+ created: function () {
+   this.updateArray();
+ }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
